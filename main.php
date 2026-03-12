c9e8958669ff10a5e44a1105d31eedd8a3fe8375
Thomas <tmagnum51@gmail.com>
2026-03-12 12:15:35 +0100

    premier commit

---
+++ main.php
@@ -0,0 +1,10 @@
+<?php
+while (true) {
+    $line = readline("Entrez votre commande : ");
+    echo "Vous avez saisi : $line\n";
+if ($line = 'list') {
+    echo "Affichage de la liste\n";   
+}
+
+ 
+}   
